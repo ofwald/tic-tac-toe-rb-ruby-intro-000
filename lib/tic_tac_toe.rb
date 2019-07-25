6cf80b9ae74acae52c0ec8c7372cfce65e1ef868
Otto Wald <ofwald@gmail.com>
2019-07-25 15:34:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,7 +116,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    puts "Congratulations #{winner[board]}!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
       puts "Cat's Game!"
   end
